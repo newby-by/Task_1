e1487e9fcc507f87011a8cc808404e1adb96ae67
newby-by <practikum10@gmail.com>
2026-04-13 21:07:00 +0300

    Add tests for Bun class

---
+++ tests/test_bun.py
@@ -0,0 +1,17 @@
+import pytest
+
+from bun import Bun
+from data import buns_data
+
+
+class TestBun:
+
+    @pytest.mark.parametrize(
+        'name, price', buns_data
+    )
+    def test_create_bun_with_expected_data(
+        self, name, price
+    ):
+        actual_bun = Bun(name, price)
+        assert (actual_bun.get_name() == name and 
+                actual_bun.get_price() == price)
