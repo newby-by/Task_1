92efb8cfce15e980e5329a45e567772b0fbe79ac
newby-by <practikum10@gmail.com>
2026-04-14 21:46:26 +0300

    Add tests for Burger class

--- conftest.py
+++ conftest.py
@@ -1,8 +1,63 @@
+import random
+
 import pytest
 
+from praktikum.burger import Burger
 from praktikum.database import Database
 
 
 @pytest.fixture(scope='function')
 def database():
     return Database()
+
+
+@pytest.fixture(scope='function')
+def bun(database):
+    return random.choice(database.buns)
+
+
+@pytest.fixture(scope='function')
+def ingredient(database):
+    return random.choice(database.ingredients)
+
+
+@pytest.fixture(scope='function')
+def burger():
+    return Burger()
+
+
+@pytest.fixture(scope='function')
+def burger_with_some_ingredients(burger, database):
+    _ = database.ingredients
+    ingredients = list(set([random.choice(_) for i in range(len(_))]))
+    [burger.add_ingredient(ingredient) for ingredient in ingredients]
+    return burger
+
+
+@pytest.fixture(scope='function')
+def burger_with_all_ingredients(burger, database):
+    burger.ingredients = database.ingredients
+    return burger
+
+
+@pytest.fixture(scope='function')
+def burger_with_some_bun(burger, bun):
+    burger.set_buns(bun)
+    return burger
+
+
+@pytest.fixture(scope='function')
+def index(burger_with_some_ingredients):
+    return random.randint(
+        0, len(burger_with_some_ingredients.ingredients) - 1
+    )
+
+
+@pytest.fixture(scope='function')
+def indexes(burger_with_all_ingredients):
+    numbers = len(burger_with_all_ingredients.ingredients)
+    list_numbers = [i for i in range(numbers)]
+    index1 = random.choice(list_numbers)
+    list_numbers.remove(index1)
+    index2 = random.choice(list_numbers)
+    return (index1, index2)
---
+++ tests/test_burger.py
@@ -0,0 +1,52 @@
+class TestBurger:
+    
+    def test_set_bun_with_expected_data(self, bun, burger):
+        burger.set_buns(bun)
+        actual_bun = burger.bun
+
+        assert (actual_bun.get_name() == bun.get_name() and
+                actual_bun.get_price() == bun.get_price())
+
+    def test_add_ingredient_with_expected_data(self, ingredient, burger):
+        ingredients_number_before = len(burger.ingredients)
+        burger.add_ingredient(ingredient)
+        ingredients_number_after = len(burger.ingredients)
+
+        assert (ingredients_number_before == ingredients_number_after - 1 and
+                burger.ingredients[-1] == ingredient)
+
+    def test_remove_ingredient_with_expected_index(
+        self, burger_with_some_ingredients, index
+    ):
+        burger = burger_with_some_ingredients
+        ingredients_number_before = len(burger.ingredients)
+        expected_ingredient = burger.ingredients[index]
+        burger.remove_ingredient(index)
+        ingredients_number_after = len(burger.ingredients)
+
+        assert (ingredients_number_before == ingredients_number_after + 1 and
+                expected_ingredient not in burger.ingredients)
+
+    def test_move_ingredient_with_diff_indexes(
+        self, burger_with_all_ingredients, indexes
+    ):
+        burger = burger_with_all_ingredients
+        ingredients = burger.ingredients
+        index1, index2 = indexes
+        ingredient_with_index1 = ingredients[index1]
+        ingredients_number_before = len(burger.ingredients)
+
+        burger.move_ingredient(index1, index2)
+        ingredient_with_index2 = ingredients[index2]
+        ingredients_number_after = len(burger.ingredients)
+        
+        assert (ingredient_with_index1 == ingredient_with_index2 and
+                ingredients_number_before == ingredients_number_after)
+
+    def test_get_price_burger_with_bun_only(
+        self, burger_with_some_bun
+    ):
+        bun = burger_with_some_bun.bun
+        actual_price = burger_with_some_bun.get_price()
+
+        assert actual_price == bun.price * 2
